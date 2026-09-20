7374d996b51cf08097538b27dd45e226dafc9b66
RevLayDust <225862333+RevLayDust@users.noreply.github.com>
2026-09-20 16:57:54 +0700

    chore(release): update patch identifier to RLUV_20260920_1653

--- __version__.py
+++ __version__.py
@@ -1,4 +1,4 @@
 VERSION = 'v0.1.0-beta'
-PATCH = 'RLUV_20260915_2010' # represent RLUV_yyyymmdd_HHMM (year, month, day, hour, minute)
+PATCH = 'RLUV_20260920_1653' # represent RLUV_yyyymmdd_HHMM (year, month, day, hour, minute)
 PATCH_MAC = 'Not yet available'
 PATCH_LINUX = 'Not yet available'
